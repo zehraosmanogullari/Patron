43f46eda8b2dfb89d0d08eb38b0373108ed7ddd6
zehraosmanogullari <zehraozcelik14@gmail.com>
2020-06-29 21:15:41 +0200

    sayin patronum ikinci calismasi ektedir

--- Bisiklet.java
+++ Bisiklet.java
@@ -4,7 +4,8 @@ public class Bisiklet {
 
 	public static void main(String[] args) {
 		System.out.println("bisiklet class`i");
-		
+		System.out.println("bugun ikinci gun cok heyecanliyim");
+		System.out.println("acaba yaptiklarimi patron begendi mi");
 
 	}
 
